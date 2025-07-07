2620c1d3e875afdebb801738589c2fe770161ad4
Shavlyuk <Shyjin98@mail.ru>
2025-07-07 14:22:37 +0300

    unicorns

--- unicorn.py
+++ unicorn.py
@@ -1,5 +1,15 @@
+unicorns = [{"color": "pink"}, {"color": "blue"}, {"color": "sparkly"}]
 def unicorns_to_rainbow(unicorns: list[dict]) -> list[str]:
+
     for i in unicorns:
-        print(i)
-    return
-unicorns = [{"color":"pink"},{"color":"blue"},{"color":"sparkly"}]
+        print(f'🌈 Rainbow unicorn of color {i['color']}')
+
+unicorns_to_rainbow(unicorns)
+
+
+# результат:
+# [
+#   "🌈 Rainbow unicorn of color pink",
+#   "🌈 Rainbow unicorn of color blue",
+#   "🌈 Rainbow unicorn of color sparkly"
+# ]
